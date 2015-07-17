ae7bb2f90b2655505a10b7bf654aba0203e20756
redwritingcode <redwritingcode@gmail.com>
2015-07-17 20:44:07 +1000

    adding responder in gemfile and adding atom feed and autodiscovery

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,10 +2,17 @@ class PostsController < ApplicationController
   before_filter :authenticate, except: [ :index, :show ]
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
-  # GET /posts
+   # GET /posts
   # GET /posts.json
+  # GET /posts.atom
   def index
     @posts = Post.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @posts }
+      format.atom
+    end
   end
 
   # GET /posts/1
