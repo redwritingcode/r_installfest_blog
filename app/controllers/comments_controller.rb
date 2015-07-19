81385acdc30471783aa9540cce651bd99bb6a509
redwritingcode <redwritingcode@gmail.com>
2015-07-19 15:04:06 +1000

    fixed comments controller post-testing

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,18 @@
+
 class CommentsController < ApplicationController
- def create
-    @post = Post.find(params[:post_id])
-    @comment = @post.comments.create!(comment_params)
+  def create
+   @post = Post.find(params[:post_id])
+   @comment = @post.comments.create!(comment_params)
     respond_to do |format|
-      format.html { redirect_to @post }
-      format.js
+    format.html { redirect_to @post }
+    format.js
     end
-  end  
+  end
+
+def comment_params
+    params.require(:comment).permit(:body)
+  end
+
 end
 
 
